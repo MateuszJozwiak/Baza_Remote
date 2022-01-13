714224315f6603a2a8ae1316da42b632a18717a0
MateuszJozwiak <94736654+MateuszJozwiak@users.noreply.github.com>
2022-01-13 19:30:47 +0100

    Add files via upload

---
+++ SpisRemote/SpisRemote/adresy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpisRemote
+{
+    class adresy
+    {/// <summary>
+    /// Klasa w ktorej skladowany jest adres globalny skad jest baza danych.
+    /// </summary>
+        private static string adres;
+        public static string Adres
+        {
+            get
+            {
+                return adres;
+            }
+            set
+            {
+                adres = value;
+            }
+        }
+    }
+}
